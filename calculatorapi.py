7d4e4d7526246b7bd6f47947c2d84470166f4b3c
Srijan Tirunagari <111377211+SrijanSinny@users.noreply.github.com>
2023-06-13 18:39:00 +0530

    Add files via upload

---
+++ calculatorapi.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+app = FastAPI()
+@app.get("/add")
+def add_numbers(num1: float, num2: float):
+    return{"result" : num1 + num2}
+@app.get("/subtract")
+def subtract_numbers(num1: float, num2: float):
+    return{"result" : num1 - num2}
+@app.get("/multiply")
+def multiply_numbers(num1: float, num2: float):
+    return{"result" : num1 * num2}
+@app.get("/divide")
+def divide_numbers(num1: float, num2: float):
+    if num2 == 0:
+        return{"errorr": "cannot divide by zero"}
+    return{"result": num1 / num2}
